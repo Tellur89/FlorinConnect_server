976d980e47ef862cf92203b79e52c9a28fdbe65f
Tom Burzynski <tellur.beryl@gmail.com>
2023-05-05 15:56:12 +0100

    add User model

--- models/User.js
+++ models/User.js
@@ -18,7 +18,7 @@ class User {
 	}
 
 	static async getOneByUsername(username) {
-		const response = await db.query('SELECT * FROM users WHERE username = $1', [username]);
+		const response = await db.query('SELECT * FROM users WHERE username = $1;', [username]);
 
 		if (response.rows.length != 1) {
 			throw new Error('Unable to locate user by username.');
@@ -26,7 +26,38 @@ class User {
 		return new User(response.rows[0]);
 	}
 
-	// static async getAdmin(){
-	//   const response =
-	// }
+	static async getAdmin() {
+		const response = await db.query('SELECT * FROM users WHERE admin = true;');
+
+		if (response.rows.length != 1) {
+			throw new Error('Unable to locate user by username.');
+		}
+		return new User(response.rows);
+	}
+
+	static async create(data) {
+		const { username, password } = data;
+		const response = await db.query('INSERT INTO users (username, password) VALUES ($1, $2) RETURNING user_id;', [username, password]);
+		const newId = response.rows[0].user_id;
+		const newUser = await User.getOneById(newId);
+		return newUser;
+	}
+
+	static async update(data, id) {
+		const { admin } = data;
+		const response = await db.query('UPDATE users SET admin = $1 WHERE users_id = $2;', [admin, id]);
+
+		if (response.rows.length != 1) {
+			throw new Error('Unable to update the user');
+		}
+		return new User(response.rows[0]);
+	}
+
+	static async destroy() {
+		const response = await db.query('DELETE FROM users WHERE user_id = $1', [this.id]);
+
+		return new User(response.rows[0]);
+	}
 }
+
+module.exports = Post;
