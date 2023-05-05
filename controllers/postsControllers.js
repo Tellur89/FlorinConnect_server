eae2d0247e9067ea4c5a48342f7d3a8ed5119e38
Tom Burzynski <tellur.beryl@gmail.com>
2023-05-05 22:13:03 +0100

    add search by date and search between dates

--- controllers/postsControllers.js
+++ controllers/postsControllers.js
@@ -1,101 +1,132 @@
 const Post = require('../models/Post');
 
-async function index (req, res) {
-    try {
-        const posts = await Post.getAll();
-        res.status(200).json(posts);
-    } catch (err) {
-        res.status(404).json({"error": err.message});
-    }
+async function index(req, res) {
+	try {
+		const posts = await Post.getAll();
+		res.status(200).json(posts);
+	} catch (err) {
+		res.status(404).json({ error: err.message });
+	}
 }
 
-async function show (req, res) {
-    try {
-        const id = parseInt(req.params.id);
-        const post = await Post.getOneById(id);
-        res.status(200).json(post);
-    } catch (err) {
-        res.status(404).json({"error": err.message})
-    }
+async function show(req, res) {
+	try {
+		const id = parseInt(req.params.id);
+		const post = await Post.getOneById(id);
+		res.status(200).json(post);
+	} catch (err) {
+		res.status(404).json({ error: err.message });
+	}
 }
 
-async function getByCategory (req, res) {
-    try {
-        const type = req.params.type
-        const posts = await Post.showByCategory(type);
-        res.status(200).json(posts);
-    } catch (err) {
-        res.status(404).json({"error": err.message});
-    }
+async function getByCategory(req, res) {
+	try {
+		const type = req.params.type;
+		const posts = await Post.showByCategory(type);
+		res.status(200).json(posts);
+	} catch (err) {
+		res.status(404).json({ error: err.message });
+	}
 }
 
-async function getByOpen (req, res) {
-    try {
-        const posts = await Post.showByOpen();
-        res.status(200).json(posts);
-    } catch (err) {
-        res.status(404).json({"error": err.message});
-    }
+//Get by date
+async function getByDate(req, res) {
+	try {
+		const date = req.params.date;
+		const posts = await Post.showByDate(date);
+		res.status(200).json(posts);
+	} catch (error) {
+		res.status(404).json({ error: error.message });
+	}
 }
 
-async function getByAccepted (req, res) {
-    try {
-        const posts = await Post.showByAccepted();
-        res.status(200).json(posts);
-    } catch (err) {
-        res.status(404).json({"error": err.message});
-    }
+//Get between dates - query needs to be send from client form
+async function getBetweenDates(req, res) {
+	try {
+		const { startDate, endDate } = req.query;
+		const posts = await Post.showBetweenDates(startDate, endDate);
+		res.status(200).json(posts);
+	} catch (error) {
+		res.status(404).json({ error: error.message });
+	}
 }
 
-async function getByCompleted (req, res) {
-    try {
-        const posts = await Post.showByCompleted();
-        res.status(200).json(posts);
-    } catch (err) {
-        res.status(404).json({"error": err.message});
-    }
+async function getByOpen(req, res) {
+	try {
+		const posts = await Post.showByOpen();
+		res.status(200).json(posts);
+	} catch (err) {
+		res.status(404).json({ error: err.message });
+	}
 }
 
-async function create (req, res) {
-    try {
-        const post = await Post.create(req.body);
-        res.status(201).json(post);
-    } catch (err) {
-        res.status(404).json({"error": err.message})
-    }
+async function getByAccepted(req, res) {
+	try {
+		const posts = await Post.showByAccepted();
+		res.status(200).json(posts);
+	} catch (err) {
+		res.status(404).json({ error: err.message });
+	}
 }
 
-async function update (req, res) {
-    try {
-        const id = parseInt(req.params.id);
-        const data = await Post.getOneById(id);
-
-        data.title = req.body.title || data.title;
-        data.content = req.body.content || data.content;
-        data.category = req.body.category || data.category;
-        data.open = req.body.open || data.open;
-        data.accepted = req.body.accepted || data.accepted;
-        data.completed  = req.body.completed || data.completed;
-        data.accepted_by_id = req.body.accepted_by_id || data.accepted_by_id;
-
-        const result = await Post.update(data);
-        res.status(200).json(result);
-    } catch (err) {
-        res.status(404).json({"error": err.message})
-    }
+async function getByCompleted(req, res) {
+	try {
+		const posts = await Post.showByCompleted();
+		res.status(200).json(posts);
+	} catch (err) {
+		res.status(404).json({ error: err.message });
+	}
 }
 
-async function destroy (req, res) {
-    try {
-        const id = parseInt(req.params.id);
-        const result = await Post.destroy(id);
-        res.json(result);
-    } catch (err) {
-        res.status(404).json({"error": err.message})
-    }
+async function create(req, res) {
+	try {
+		const post = await Post.create(req.body);
+		res.status(201).json(post);
+	} catch (err) {
+		res.status(404).json({ error: err.message });
+	}
 }
 
+async function update(req, res) {
+	try {
+		const id = parseInt(req.params.id);
+		const data = await Post.getOneById(id);
+
+		data.title = req.body.title || data.title;
+		data.content = req.body.content || data.content;
+		data.category = req.body.category || data.category;
+		data.open = req.body.open || data.open;
+		data.accepted = req.body.accepted || data.accepted;
+		data.completed = req.body.completed || data.completed;
+		data.accepted_by_id = req.body.accepted_by_id || data.accepted_by_id;
+
+		const result = await Post.update(data);
+		res.status(200).json(result);
+	} catch (err) {
+		res.status(404).json({ error: err.message });
+	}
+}
+
+async function destroy(req, res) {
+	try {
+		const id = parseInt(req.params.id);
+		const result = await Post.destroy(id);
+		res.json(result);
+	} catch (err) {
+		res.status(404).json({ error: err.message });
+	}
+}
 
 module.exports = {
-    index, show, create, getByCategory, getByOpen, getByCompleted, getByAccepted, update, destroy
-}
+	index,
+	show,
+	create,
+	getByCategory,
+	getByOpen,
+	getByCompleted,
+	getByAccepted,
+	update,
+	destroy,
+	getBetweenDates,
+	getByDate,
+};
--- routers/postRoutes.js
+++ routers/postRoutes.js
@@ -1,24 +1,19 @@
 const { Router } = require('express');
 
-const postControllers  = require('../controllers/postsControllers');
+const postControllers = require('../controllers/postsControllers');
 
 const postRouters = Router();
 
-postRouters.route('/')
-    .get(postControllers.index)
-    .post(postControllers.create);
+postRouters.route('/').get(postControllers.index).post(postControllers.create);
 
 postRouters
-    .get('/category/:type', postControllers.getByCategory)
-    .get('/status/open', postControllers.getByOpen)
-    .get('/status/accepted', postControllers.getByAccepted)
-    .get('/status/completed', postControllers.getByCompleted)
+	.get('/category/:type', postControllers.getByCategory)
+	.get('/status/open', postControllers.getByOpen)
+	.get('/status/accepted', postControllers.getByAccepted)
+	.get('/status/completed', postControllers.getByCompleted)
+	.get('/date/:date', postControllers.getByDate)
+	.get('/date/search', postControllers.getBetweenDates);
 
-postRouters.route('/:id')
-    .get(postControllers.show)
-    .patch(postControllers.update)
-    .delete(postControllers.destroy);
-    
-
+postRouters.route('/:id').get(postControllers.show).patch(postControllers.update).delete(postControllers.destroy);
 
 module.exports = postRouters;
