6986e9052311a66a82d2d61a100ebf5e0a8b9071
Tom Burzynski <tellur.beryl@gmail.com>
2023-05-11 20:25:06 +0100

    implement token

--- app.js
+++ app.js
@@ -23,6 +23,8 @@ const app = express();
 // };
 app.use(cors());
 app.use(express.json());
+app.use('/users', userRoutes);
+app.use('/posts', postRouters);
 // app.use(express.urlencoded({ extended: false }));
 
 // app.use(cookieParser());
@@ -43,7 +45,5 @@ app.get('/', (req, res) => {
 // anything below this will need authorization
 
 // app.use(verifyJWT);
-app.use('/posts', postRouters);
-app.use('/users', userRoutes);
 
 module.exports = app;
