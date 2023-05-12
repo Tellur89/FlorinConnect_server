b884702c94c6d5b22ce44ba6e2bd9f4ad99c6268
AjayaBudha <ajaya.budhathoki@hotmail.com>
2023-05-12 11:19:42 +0100

    add final versions of testing units

--- __testing__/contollers_testing/postControllers.test.js
+++ __testing__/contollers_testing/postControllers.test.js
@@ -14,7 +14,32 @@ describe("api server", () => {
     api.close(done);
   });
 
-  test("testing this", async () => {
+  //GET tests
+  test("requesting for a request code of 200 when post route sends all the posts", async () => {
     await request(app).get("/posts").expect(200);
+  });
+  test("requesting for a request code of 200 when post route sends all the volunteer posts", async () => {
+    await request(app).get("posts/category/Volunteer").expect(200);
+  });
+  test("requesting for a request code of 200 when post route sends all the posts that are open", async () => {
+    await request(app).get("posts/status/open").expect(200);
+  });
+  test("requesting for a request code of 200 when post route sends all the posts that are completed", async () => {
+    await request(app).get("posts/status/completed").expect(200);
+  });
+
+  test("requesting for a request code of 200 when post route sends all the posts that are accepted", async () => {
+    await request(app).get("posts/status/accepted").expect(200);
+  });
+
+  test("requesting for a request code of 200 when post route sends all the posts that are on this day", async () => {
+    await request(app).get("posts/date/12-05-2023").expect(200);
+  });
+
+  test("requesting for a request code of 200 when post route sends all the posts that are between 11/05/2023 and 12/05/2023", async () => {
+    await request(app).get("posts/date/11-05-2023").expect(200);
+  });
+  test("requesting for a request code of 200 when post route sends all the posts that inclue the word Junior", async () => {
+    await request(app).get("posts/word/Junior").expect(200);
   });
 });
