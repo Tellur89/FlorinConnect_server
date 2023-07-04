7625c3c96416dcb096cfd482b8d2ce6e4834ce10
Tom Burzynski <tellur.beryl@gmail.com>
2023-07-04 22:56:35 +0100

    add testing and coverage

--- __testing__/models_testing/User.test.js
+++ __testing__/models_testing/User.test.js
@@ -1,16 +1,16 @@
-const User = require("../../models/User");
-const { getOneById } = require("../../controllers/usersController");
-describe("User Model", () => {
-  test("is described", () => {
-    expect(User).toBeDefined();
-  });
+const User = require('../../models/User');
+const { getOneById } = require('../../controllers/usersController');
+describe('User Model', () => {
+	test('is described', () => {
+		expect(User).toBeDefined();
+	});
 
-  test("user.all exist", () => {
-    const user = User.getAll;
-    expect(user).toBeDefined();
-  });
-  test("user.all exist", () => {
-    const user = User.all();
-    expect(user.value).toBe(0).expect("No users in database");
-  });
+	test('user all exist', () => {
+		const user = User.getAll;
+		expect(user).toBeDefined();
+	});
+	test('user.all exist', () => {
+		const user = User.getAll();
+		expect(user.value).toBe(0).expect('No users in database');
+	});
 });
